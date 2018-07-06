1bebaeca8a9dc51f176d41e205d5e9ce327f4ff7
mdss@makexhtml.ru <mdss@makexhtml.ru>
2018-07-06 21:15:37 +0300

    final 320 homepage

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,13 @@
 (function($){
 	"use strict";
 
+	// certificates carousel
 	$('.js-cert-list').slick();
+
+	// reviews carousel
+	$('.js-homepage-reviews').slick();
+
+	// showroom carousel
+	$('.js-homepage-showroom').slick();
 
 })(jQuery);

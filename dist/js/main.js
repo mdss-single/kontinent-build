acad708ccde42419baba3c2851bf08cbbd3e1a88
mdss@makexhtml.ru <mdss@makexhtml.ru>
2018-07-07 22:07:38 +0300

    480 screen

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,6 +8,29 @@
 	$('.js-homepage-reviews').slick();
 
 	// showroom carousel
-	$('.js-homepage-showroom').slick();
+	$('.js-homepage-showroom').slick({
+		slidesToScroll: 1,
+		mobileFirst: true,
+		responsive: [
+		{
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 1279,
+			settings: {
+				slidesToShow: 4,
+			}
+		}
+		]
+	});
 
 })(jQuery);

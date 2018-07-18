5229e6d9b0218711e3334be20eef966427b68aab
mdss@makexhtml.ru <mdss@makexhtml.ru>
2018-07-18 21:55:37 +0300

    где купить 320, фиксированоое меню, выезжающее боковое меню

--- dist/js/main.js
+++ dist/js/main.js
@@ -77,4 +77,49 @@
 		]
 	});
 
+	// show menu
+	$('.js-menu').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		showMenu();
+	});
+	$(document).on('click', function(e) {
+		if (!$(e.target).closest('.nav__main').length) {
+			hideMenu();
+		}
+	});
+
+	// sticky nav
+	$('.nav').stick_in_parent({
+		parent: 'body',
+	});
+
+	// doubt types
+	$('.js-doubt-compare .doubt__types').each(function(i) {
+		var storage = localStorage.getItem('tab' + i);
+		if (storage) {
+			$(this).find('.doubt__type').removeClass('doubt__type--active').eq(storage).addClass('doubt__type--active').closest('.doubt__compare').find('.doubt__content').removeClass('doubt__content--active').eq(storage).addClass('doubt__content--active');
+		}
+	});
+	$('.js-doubt-compare .doubt__types').on('click', '.doubt__type:not(.doubt__type--active)', function() {
+		$(this).addClass('doubt__type--active').siblings().removeClass('doubt__type--active').closest('.doubt__compare').find('.doubt__content').removeClass('doubt__content--active').eq($(this).index()).addClass('doubt__content--active');
+		var ulIndex = $('.js-doubt-compare .doubt__types').index($(this).parents('.js-doubt-compare .doubt__types'));
+		localStorage.removeItem('tab' + ulIndex);
+		localStorage.setItem('tab' + ulIndex, $(this).index());
+	});
+
+	function showMenu() {
+		if (!$('.nav__cover').length) $('body').prepend('<div class="nav__cover"></div>');
+		$('html').addClass('no-scroll');
+		$('.nav__main').addClass('nav__main--active');
+	}
+
+	function hideMenu() {
+		if ($('html').hasClass('no-scroll')) $('html').removeClass('no-scroll');
+		if ($('.nav__cover').length) {
+			$('.nav__cover').remove();
+		}
+		$('.nav__main').removeClass('nav__main--active');
+	}
+
 })(jQuery);
